0a59bb431b4a0f7051b8e14b884bab00fb188f9f
liangshuo276 <liangshuo276@heoobike.com>
2023-02-25 21:37:18 +0800

    feat(struct):字符串&数组

---
+++ Struct/02_string.py
@@ -0,0 +1,73 @@
+"""
+字符串
+    不能被修改，只能重新定义
+"""
+# 定义
+str1 = "hello world!"
+
+# 遍历
+for i in range(len(str1)):
+    print(str1[i], end=" ")  # h e l l o   w o r l d !
+
+for item in str1:
+    print(item, end=" ")  # h e l l o   w o r l d !
+
+"""
+增删改查
+"""
+
+# 增
+str2 = str1 + " let go \n"
+str3 = "%s %s %s" % (str1, "hello code!", 33)
+str4 = "I am {}. I am {} ".format("alex", 29)
+print(str2)  # hello world! let go
+print(str3)  # hello world! hello code! 33
+print(str4)  # I am alex. I am 29
+
+# 删
+new_str = ""
+for i in range(len(str1)):
+    if str1[i] == "l":
+        continue
+    new_str += str1[i]
+print(new_str)  # heo word!
+
+# 改
+new_str1 = ""
+for i in range(len(str1)):
+    if str1[i] == "l":
+        new_str1 += "L"
+    else:
+        new_str1 += str1[i]
+print(new_str1)  # heLLo worLd!
+print(new_str1.replace("h", "H"))  # HeLLo worLd!
+
+# 查
+is_exist = False
+for i in range(len(str1)):
+    if str1[i] == "!":
+        is_exist = True
+print(is_exist)  # True
+
+print(str1.find("d"), str1[str1.find("d")], str1.find("d") != -1)  # 10 d True
+
+"""
+方法
+"""
+# 拼接、格式化
+str2 = str1 + " let go \n"
+str3 = "%s %s %s" % (str1, "hello code!", 33)
+str4 = "I am {}. I am {} ".format("alex", 29)
+
+# 生成字符串
+array1 = ["1.1.1.1", "3306"]
+ip_port = ":".join(array1)
+print(ip_port)  # 1.1.1.1:3306
+
+# 反转
+str8 = "hello world!"
+print(str8[::-1])  # !dlrow olleh
+
+# split
+arr8 = str8.split()
+print(arr8)  # ['hello', 'world!']
---
+++ Struct/03_array.py
@@ -0,0 +1,89 @@
+"""
+数组array
+"""
+
+# 定义
+arr = [11, 22, 33, 44, 55]
+
+# 遍历
+for i in range(len(arr)):
+    print(arr[i], end=" ")  # 11 22 33 44 55
+
+for item in arr:
+    print(item, end=" ")
+
+for index, val in enumerate(arr):  # [ɪˈnuːməreɪt]
+    print(index, val, end=" ")
+
+"""
+增删改查
+"""
+# 增
+arr1 = ["app_name"]
+
+# append 尾部追加
+arr1.append("level")
+print(arr1)  # ['app_name', 'level']
+
+# extend 合并
+arr2 = ["create_time", "update_time"]
+arr1.extend(arr2)
+print(arr1)  # ['app_name', 'level', 'create_time', 'update_time']
+
+# insert 第i个下标处，增加
+arr1.insert(0, "id")
+print(arr1)  # ['id', 'app_name', 'level', 'create_time', 'update_time']
+
+new_arr = []
+for i in range(len(arr1)):
+    if i == 4:
+        new_arr.append("lang")
+    new_arr.append(arr1[i])
+print(new_arr)  # ['id', 'app_name', 'level', 'create_time', 'lang', 'update_time']
+
+# 删
+new_arr2 = []
+for item in arr1:
+    if item == "app_lang":
+        continue
+    new_arr2.append(item)
+print(new_arr2)  # ['id', 'app_name', 'level', 'create_time', 'update_time']
+
+del new_arr2[0]
+print(new_arr2)  # ['app_name', 'level', 'create_time', 'update_time']
+
+del new_arr2[1:2]  # [1,2)  删除第一个元素
+print(new_arr2)  # ['app_name', 'create_time', 'update_time']
+
+arr1.pop()
+print(arr1)   # ['id', 'app_name', 'level', 'create_time']
+
+arr1.remove("create_time")
+print(arr1)  # ['id', 'app_name', 'level']
+
+# 改
+for i in range(len(arr1)):
+    if arr1[i] == "app_name":
+        arr1[i] = "APP_NAME"
+print(arr1)  # ['id', 'APP_NAME', 'level']
+
+# 查
+target_index = arr1.index("level")   # 查询该元素的下标
+print(target_index)   # 2
+
+is_exist = "level" in arr1
+print(is_exist)  # True
+
+is_exist = False
+for i in range(len(arr1)):
+    if arr1[i] == "is_sea":
+        is_exist = True
+        break
+print(is_exist)  # False
+
+"""
+方法
+"""
+# join拼接
+str1 = ",".join(arr1)
+print(str1)  # id,APP_NAME,level
